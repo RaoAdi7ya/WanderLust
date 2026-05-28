c9d8badfe7fe53aa0a9c0329de9fb68ff7e3dbdc
Aditya yadav <yaduvanshiaditya4200@gmail.com>
2026-05-28 18:48:56 +0530

    added image preview for edit page

--- controllers/listings.js
+++ controllers/listings.js
@@ -44,7 +44,10 @@ module.exports.renderEditForm = async (req, res) => {
       req.flash("error", "Cannot find that listing!");
       return res.redirect("/listings");
     }
-    res.render("listings/edit.ejs", { listing });
+
+   let originalImageUrl = listing.image.url;
+   originalImageUrl = originalImageUrl.replace("/upload/", "/upload/w_250/");
+    res.render("listings/edit.ejs", { listing, originalImageUrl });
   };
   //update route
 module.exports.updateListing = async (req, res) => {
