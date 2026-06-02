ae2c2e083615650d37c57b82873b3e7904708ab1
Aditya yadav <yaduvanshiaditya4200@gmail.com>
2026-06-02 08:34:56 +0530

    fixed an error

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const WrapAsync = require("../utils/WrapAsync");
+const WrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware");
 
